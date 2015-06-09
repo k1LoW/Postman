424c2985ebad7e3fe35ff3091c7e5dc477e43901
k1LoW <k1lowxb@gmail.com>
2015-06-09 21:20:33 +0900

    Add testcase

--- Test/Case/Lib/Network/Email/RegisteredMailTest.php
+++ Test/Case/Lib/Network/Email/RegisteredMailTest.php
@@ -1,23 +1,52 @@
 <?php
 
 App::uses('RegisteredMail', 'Postman.Network/Email');
+App::uses('HttpSocket', 'Network/Http');
 
 class RegisteredMailTest extends CakeTestCase
 {
-    public function setUp() {
-        $this->email = new RegisteredMail();
-    }
+  public function setUp() {
+    $this->email = new RegisteredMail();
+    $this->email->config(array(
+      'transport' => 'Smtp',
+      'from' => array('form@mailback.me' => 'from'),
+      'to' => 'to+postman@fusic.co.jp',
+      'host' => 'mail.mailback.me',
+      'port' => 25,
+      'timeout' => 30,
+      'log' => false,
+      'charset' => 'utf-8',
+      'headerCharset' => 'utf-8',
+    ));
+  }
 
-    public function tearDown() {
-    }
+  public function tearDown() {
+    unset($this->email);
+  }
 
-    /**
-     * testWordWrap
-     *
-     */
-    public function testWordwrap(){
-        $str = '<!DOCTYPE html><html lang="en" class=""><head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#"><meta charset="utf-8"></html>';
-        $wrapped = explode("\n", $this->email->mb_wordwrap($str, 19, "\n", true));
-        $this->assertIdentical($wrapped[0], '<!DOCTYPE html>');
-    }
+  /**
+   * test_longMultibyteLine
+   *
+   */
+  public function test_longMultibyteLine(){
+    $message = '寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助';
+
+    $expect = $message;
+    $hash = sha1(time());
+    $this->email->to('to+'.$hash.'@mailback.me');
+    $this->email->subject('メールタイトル');
+    $this->email->send($expect);
+
+    sleep(2);
+
+    $url = 'http://mailback.me/to/'.$hash.'.body';
+    App::uses('HttpSocket', 'Network/Http');
+    $HttpSocket = new HttpSocket();
+    $results = $HttpSocket->get($url, array());
+
+    $body = trim(str_replace(array("\r\n", "\n", ' '), '', $results->body));
+    $message = trim(str_replace(array("\r\n", "\n", ' '), '', $message));
+
+    $this->assertIdentical($body, $message);
+  }
 }
