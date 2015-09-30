c361d4503c8a91bf73f3ae6ccf8a32d3a393c16d
k1LoW <k1lowxb@gmail.com>
2015-09-30 18:47:23 +0900

    Opppppssss

--- Test/Case/Lib/Network/Email/CensorshipModeTest.php
+++ Test/Case/Lib/Network/Email/CensorshipModeTest.php
@@ -73,13 +73,13 @@ class CensorshipModeTest extends CakeTestCase
         $this->assertIdentical($body, $message);
 
         // CC
-        $this->assertIdentical($this->email->cc(), array());
         $ccUrl = 'http://mailback.me/to/unknown-cc-'.$hash.'.body';
+        $results = $HttpSocket->get($ccUrl, array());
         $this->assertIdentical($results->code, '404');
         
         // BCC
-        $this->assertIdentical($this->email->bcc(), array());
         $bccUrl = 'http://mailback.me/to/unknown-bcc-'.$hash.'.body';
+        $results = $HttpSocket->get($bccUrl, array());
         $this->assertIdentical($results->code, '404');
     }
 }
