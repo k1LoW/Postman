11231e618537acb8f266bee3db93ac5f3f45a024
k1LoW <k1lowxb@gmail.com>
2015-09-30 17:08:21 +0900

    Add testcase for red

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Postman [![Build Status](https://travis-ci.org/k1LoW/Postman.svg?branch=master)](https://travis-ci.org/k1LoW/Postman)
+
+## Censorship mode
+
+```php
+Configure::write('Postman.censorship.mode', true);
+Configure::write('Postman.censorship.config', array(
+    'transport' => 'Smtp',
+    'from' => array('form@mailback.me' => 'from'),
+    'to' => 'to+postman@fusic.co.jp',
+    'host' => 'mail.mailback.me',
+    'port' => 25,
+    'timeout' => 30,
+    'log' => false,
+    'charset' => 'utf-8',
+    'headerCharset' => 'utf-8',
+));
+```
---
+++ Test/Case/Lib/Network/Email/CensorshipModeTest.php
@@ -0,0 +1,71 @@
+<?php
+
+App::uses('RegisteredMail', 'Postman.Network/Email');
+App::uses('HttpSocket', 'Network/Http');
+
+class CensorshipModeTest extends CakeTestCase
+{
+  public $fixtures = array(
+    'plugin.postman.registered_mail_log',
+  );
+
+  public function setUp() {
+    $this->email = new RegisteredMail();
+    $this->email->config(array(
+      'transport' => 'Smtp',
+      'from' => array('form@example.com' => 'from'),
+      'to' => 'to+unknown-to@mailback.me',
+      'cc' => 'to+unknown-cc@mailback.me',
+      'bcc' => 'to+unknown-bcc@mailback.me',
+      'host' => 'mail.mailback.me',
+      'port' => 25,
+      'timeout' => 30,
+      'log' => false,
+      'charset' => 'utf-8',
+      'headerCharset' => 'utf-8',
+    ));
+  }
+
+  public function tearDown() {
+    unset($this->email);
+  }
+
+  /**
+   * test_CensorshipOn
+   *
+   */
+  public function test_Censorship(){
+      $hash = sha1(uniqid('',true));
+      $to = 'to+'.$hash.'@mailback.me';
+      
+      Configure::write('Postman.censorship.mode', true);      
+      Configure::write('Postman.censorship.config', array(
+          'transport' => 'Smtp',
+          'from' => array('form@mailback.me' => 'from'),
+          'to' => $to,
+          'host' => 'mail.mailback.me',
+          'port' => 25,
+          'timeout' => 30,
+          'log' => false,
+          'charset' => 'utf-8',
+          'headerCharset' => 'utf-8',
+      ));
+
+      $message = 'Censored';
+      $this->email->subject('メールタイトル');
+      $this->email->send($message);
+
+      sleep(5);
+
+      $url = 'http://mailback.me/to/'.$hash.'.body';
+
+      App::uses('HttpSocket', 'Network/Http');
+      $HttpSocket = new HttpSocket();
+      $results = $HttpSocket->get($url, array());
+      
+      $body = trim(str_replace(array("\r\n", "\n", ' '), '', $results->body));
+      $message = trim(str_replace(array("\r\n", "\n", ' '), '', $message));
+
+      $this->assertIdentical($body, $message);
+  }
+}
