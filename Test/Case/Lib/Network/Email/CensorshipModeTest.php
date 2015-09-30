b56dd195534b86b9d354c1975915c309f3c9dfc9
k1LoW <k1lowxb@gmail.com>
2015-09-30 17:22:30 +0900

    Set Configure::write('Postman.censorship.mode', false);

--- Test/Case/Lib/Network/Email/CensorshipModeTest.php
+++ Test/Case/Lib/Network/Email/CensorshipModeTest.php
@@ -5,68 +5,69 @@ App::uses('HttpSocket', 'Network/Http');
 
 class CensorshipModeTest extends CakeTestCase
 {
-  public $fixtures = array(
-    'plugin.postman.registered_mail_log',
-  );
+    public $fixtures = array(
+        'plugin.postman.registered_mail_log',
+    );
 
-  public function setUp() {
-    $this->email = new RegisteredMail();
-    $this->email->config(array(
-      'transport' => 'Smtp',
-      'from' => array('form@example.com' => 'from'),
-      'to' => 'to+unknown-to@mailback.me',
-      'cc' => 'to+unknown-cc@mailback.me',
-      'bcc' => 'to+unknown-bcc@mailback.me',
-      'host' => 'mail.mailback.me',
-      'port' => 25,
-      'timeout' => 30,
-      'log' => false,
-      'charset' => 'utf-8',
-      'headerCharset' => 'utf-8',
-    ));
-  }
+    public function setUp() {
+        Configure::write('Postman.censorship.mode', false);
+        $this->email = new RegisteredMail();
+        $this->email->config(array(
+            'transport' => 'Smtp',
+            'from' => array('form@example.com' => 'from'),
+            'to' => 'to+unknown-to@mailback.me',
+            'cc' => 'to+unknown-cc@mailback.me',
+            'bcc' => 'to+unknown-bcc@mailback.me',
+            'host' => 'mail.mailback.me',
+            'port' => 25,
+            'timeout' => 30,
+            'log' => false,
+            'charset' => 'utf-8',
+            'headerCharset' => 'utf-8',
+        ));
+    }
 
-  public function tearDown() {
-    unset($this->email);
-  }
+    public function tearDown() {
+        unset($this->email);
+    }
 
-  /**
-   * test_CensorshipOn
-   *
-   */
-  public function test_Censorship(){
-      $hash = sha1(uniqid('',true));
-      $to = 'to+'.$hash.'@mailback.me';
+    /**
+     * test_CensorshipOn
+     *
+     */
+    public function test_Censorship(){
+        $hash = sha1(uniqid('',true));
+        $to = 'to+'.$hash.'@mailback.me';
 
-      Configure::write('Postman.censorship.mode', true);
-      Configure::write('Postman.censorship.config', array(
-          'transport' => 'Smtp',
-          'from' => array('form@mailback.me' => 'from'),
-          'to' => $to,
-          'host' => 'mail.mailback.me',
-          'port' => 25,
-          'timeout' => 30,
-          'log' => false,
-          'charset' => 'utf-8',
-          'headerCharset' => 'utf-8',
-      ));
+        Configure::write('Postman.censorship.mode', true);
+        Configure::write('Postman.censorship.config', array(
+            'transport' => 'Smtp',
+            'from' => array('form@mailback.me' => 'from'),
+            'to' => $to,
+            'host' => 'mail.mailback.me',
+            'port' => 25,
+            'timeout' => 30,
+            'log' => false,
+            'charset' => 'utf-8',
+            'headerCharset' => 'utf-8',
+        ));
 
-      $message = 'Censored';
-      $expect = $message;
-      $this->email->subject('メールタイトル');
-      $this->email->send($expect);
+        $message = 'Censored';
+        $expect = $message;
+        $this->email->subject('メールタイトル');
+        $this->email->send($expect);
 
-      sleep(5);
+        sleep(5);
 
-      $url = 'http://mailback.me/to/'.$hash.'.body';
+        $url = 'http://mailback.me/to/'.$hash.'.body';
 
-      App::uses('HttpSocket', 'Network/Http');
-      $HttpSocket = new HttpSocket();
-      $results = $HttpSocket->get($url, array());
+        App::uses('HttpSocket', 'Network/Http');
+        $HttpSocket = new HttpSocket();
+        $results = $HttpSocket->get($url, array());
 
-      $body = trim(str_replace(array("\r\n", "\n", ' '), '', $results->body));
-      $message = trim(str_replace(array("\r\n", "\n", ' '), '', $message));
+        $body = trim(str_replace(array("\r\n", "\n", ' '), '', $results->body));
+        $message = trim(str_replace(array("\r\n", "\n", ' '), '', $message));
 
-      $this->assertIdentical($body, $message);
-  }
+        $this->assertIdentical($body, $message);
+    }
 }
--- Test/Case/Lib/Network/Email/RegisteredMailTest.php
+++ Test/Case/Lib/Network/Email/RegisteredMailTest.php
@@ -5,52 +5,53 @@ App::uses('HttpSocket', 'Network/Http');
 
 class RegisteredMailTest extends CakeTestCase
 {
-  public $fixtures = array(
-    'plugin.postman.registered_mail_log',
-  );
+    public $fixtures = array(
+        'plugin.postman.registered_mail_log',
+    );
 
-  public function setUp() {
-    $this->email = new RegisteredMail();
-    $this->email->config(array(
-      'transport' => 'Smtp',
-      'from' => array('form@mailback.me' => 'from'),
-      'to' => 'to+postman@fusic.co.jp',
-      'host' => 'mail.mailback.me',
-      'port' => 25,
-      'timeout' => 30,
-      'log' => false,
-      'charset' => 'utf-8',
-      'headerCharset' => 'utf-8',
-    ));
-  }
+    public function setUp() {
+        Configure::write('Postman.censorship.mode', false);
+        $this->email = new RegisteredMail();
+        $this->email->config(array(
+            'transport' => 'Smtp',
+            'from' => array('form@mailback.me' => 'from'),
+            'to' => 'to+postman@fusic.co.jp',
+            'host' => 'mail.mailback.me',
+            'port' => 25,
+            'timeout' => 30,
+            'log' => false,
+            'charset' => 'utf-8',
+            'headerCharset' => 'utf-8',
+        ));
+    }
 
-  public function tearDown() {
-    unset($this->email);
-  }
+    public function tearDown() {
+        unset($this->email);
+    }
 
-  /**
-   * test_longMultibyteLine
-   *
-   */
-  public function test_longMultibyteLine(){
-    $message = '寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助';
+    /**
+     * test_longMultibyteLine
+     *
+     */
+    public function test_longMultibyteLine(){
+        $message = '寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助寿限無、寿限無五劫の擦り切れ海砂利水魚の水行末 雲来末 風来末食う寝る処に住む処藪ら柑子の藪柑子パイポパイポ パイポのシューリンガンシューリンガンのグーリンダイグーリンダイのポンポコピーのポンポコナーの長久命の長助';
 
-    $expect = $message;
-    $hash = sha1(uniqid('',true));
-    $this->email->to('to+'.$hash.'@mailback.me');
-    $this->email->subject('メールタイトル');
-    $this->email->send($expect);
+        $expect = $message;
+        $hash = sha1(uniqid('',true));
+        $this->email->to('to+'.$hash.'@mailback.me');
+        $this->email->subject('メールタイトル');
+        $this->email->send($expect);
 
-    sleep(5);
+        sleep(5);
 
-    $url = 'http://mailback.me/to/'.$hash.'.body';
-    App::uses('HttpSocket', 'Network/Http');
-    $HttpSocket = new HttpSocket();
-    $results = $HttpSocket->get($url, array());
+        $url = 'http://mailback.me/to/'.$hash.'.body';
+        App::uses('HttpSocket', 'Network/Http');
+        $HttpSocket = new HttpSocket();
+        $results = $HttpSocket->get($url, array());
 
-    $body = trim(str_replace(array("\r\n", "\n", ' '), '', $results->body));
-    $message = trim(str_replace(array("\r\n", "\n", ' '), '', $message));
+        $body = trim(str_replace(array("\r\n", "\n", ' '), '', $results->body));
+        $message = trim(str_replace(array("\r\n", "\n", ' '), '', $message));
 
-    $this->assertIdentical($body, $message);
-  }
+        $this->assertIdentical($body, $message);
+    }
 }
